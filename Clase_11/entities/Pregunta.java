e3bbe13adb534e17da1084ebe83975008eaad2ec
Ezequiel <87555292+rretta@users.noreply.github.com>
2023-06-20 18:46:34 -0300

    Create Pregunta.java

---
+++ Clase_11/entities/Pregunta.java
@@ -0,0 +1,53 @@
+package atl.course.trivia.entities;
+
+public class Pregunta {
+
+    private String category;
+    private String question;
+    private String[] options;
+    private Integer answer;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
+    }
+
+    public Integer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Integer answer) {
+        this.answer = answer;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    private String explanation;
+
+
+}
