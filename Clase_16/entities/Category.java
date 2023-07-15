3d2b97b6201edc686803f94a0f24e64ff91a9335
Ezequiel <87555292+rretta@users.noreply.github.com>
2023-07-15 01:47:56 -0300

    Create Category.java

---
+++ Clase_16/entities/Category.java
@@ -0,0 +1,21 @@
+package atl.course.trivia.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "categories")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+
+
+}
